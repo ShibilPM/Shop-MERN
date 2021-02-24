acdabd138b56136d8f63ab65bf17c39ea1541249
ShibilPM <shibilpm3232@gmail.com>
2021-02-24 12:53:44 +0530

    create back end an d fetch data from back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/header/Header.component";
 import Footer from "./components/footer/Footer.component";
 import HomePage from "./pages/homepage/HomePage.component";
+import ProductPage from "./pages/productpage/ProductPage.component";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Route path="/" component={HomePage} exact />
+          <Route path="/product/:id" component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 

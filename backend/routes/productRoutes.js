0ef7048f6d7eabd7704a955fc13252610eaf473e
ShibilPM <shibilpm3232@gmail.com>
2021-02-27 13:00:29 +0530

    seperate conrollers in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,13 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
-import Product from "../model/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productControllers.js";
 
-// @ desc    Fetch All Products
-// @ route   GET /api/products
-// @ access  Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  })
-);
-
-// @ desc    Fetch Product
-// @ route   GET /api/products/:id
-// @ access  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.status(200).json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;

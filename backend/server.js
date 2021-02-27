e387fce5305a5b1b89f6060f0744b0d2f4636e00
ShibilPM <shibilpm3232@gmail.com>
2021-02-27 13:47:57 +0530

    start with user auth

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import colors from "colors";
 
-import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import Product from "./model/productModel.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -13,7 +14,10 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
@@ -22,8 +26,6 @@ app.use(errorHandler);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 

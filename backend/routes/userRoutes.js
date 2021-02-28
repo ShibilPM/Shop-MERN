1c92baf0d6dbfde184cda8c7cd8e1342d5680f67
ShibilPM <shibilpm3232@gmail.com>
2021-02-28 15:05:13 +0530

    complete user auth back end

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
-import { authUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
